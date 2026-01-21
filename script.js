d8d62d937cc19ede92892db7c93d2a3d22efe0b3
harshkuche23 <harshkuche23@gmail.com>
2026-01-21 23:28:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+    const now = new Date();
+    timer.innerText = now.toString();
+}
+
+// Initial call
+updateTimer();
+
+// Update every second
+setInterval(updateTimer, 1000);
